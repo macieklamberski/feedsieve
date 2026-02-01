75f8cff41a10f402fba5c7f7fe699ac67895085e
Maciej Lamberski <maciej@lamberski.com>
2026-02-02 00:46:44 +0100

    refactor: Trim public API to classifyItems + consumer types only

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 export { classifyItems } from './classifier.js'
-export { computeItemHashes, normalizeLinkForHashing } from './hashes.js'
-export {
-  buildAllKeys,
-  computeAllHashes,
-  deduplicateByBatchKey,
-  detectCollisions,
-  filterWithIdentifier,
-} from './pipeline.js'
 export type {
   ClassificationResult,
   HashableItem,
-  HashedFeedItem,
   InsertAction,
   ItemHashes,
-  KeyedFeedItem,
   MatchableItem,
   UpdateAction,
 } from './types.js'
