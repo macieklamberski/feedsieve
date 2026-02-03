8f0311756f837fc5e6b8e821a319475aaecb4368
Maciej Lamberski <maciej@lamberski.com>
2026-02-03 10:38:43 +0100

    refactor: Replace Bun.CryptoHasher with node:crypto

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,7 @@
+import { createHash } from 'node:crypto'
+
 export const generateHash = (...values: Array<string | null | undefined>) => {
-  return new Bun.CryptoHasher('sha256').update(values.join('\0')).digest('hex').slice(0, 32)
+  return createHash('sha256').update(values.join('\0')).digest('hex').slice(0, 32)
 }
 
 export const isDefined = <T>(value: T | null | undefined): value is T => {
